12b1a3ebba51f3322080e2e2d725551fb6a48f52
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-06-19 16:56:47 +0900

    add functions

--- exercises/02_functions/functions4.rs
+++ exercises/02_functions/functions4.rs
@@ -6,7 +6,7 @@ fn is_even(num: i64) -> bool {
 }
 
 // TODO: 関数の入出力の記載内容を修正してください。
-fn sale_price(price: i64) -> {
+fn sale_price(price: i64) -> i64 {
     if is_even(price) {
         price - 10
     } else {
