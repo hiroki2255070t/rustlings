8a660aaa3066e53e3dc3ebde7a5558ddc27dadfd
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-06-19 16:57:40 +0900

    add quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -7,8 +7,17 @@
 // - りんご1個は2 rustbucksかかります
 // - 40個より多くリンゴを買ったら、りんご1個の値段は1 rustbucksになります。
 
+use core::num;
+
 // TODO: 与えられたりんごの注文数の金額を計算する関数を書いてください。
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(num_apple: u32) -> u32 {
+    if num_apple > 40 {
+        1 * num_apple
+    } else {
+        2 * num_apple
+    }
+}
 
 fn main() {
     // この行で関数のテストができます。
